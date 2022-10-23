f4614c30ec2908d092a62813d48887bfc5b48716
Alan <aland96>
2022-10-23 19:38:44 +0200

    Start Site, add first navbar

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+    background: transparent;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    font-weight: 700;
+`
+
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Nav } from './NavbarElements'
+
+export const Navbar = () => {
+    return (
+        <>
+            <Nav>Nav</Nav>
+        </>
+    )
+}
